a212b4f12c52d8b4c77cdaf96e8f6fabdead7b4e
Michal Rumanek <michal.rumanek@pl.abb.com>
2013-10-29 15:12:37 +0100

    callback on failure

--- www/fileopener.js
+++ www/fileopener.js
@@ -1,12 +1,15 @@
 // forked from https://github.com/markeeftb/FileOpener
 
 module.exports = {
-    open: function (url) {
+    open: function (url, onFailure) {
         var success = function () {
             console.log("success!");
         }
         var failure = function (error) {
             console.log(error);
+			if(typeof onFailure === "function") {
+				onFailure(error);
+			}
         }
         cordova.exec(success, failure, "FileOpener", "openFile", [url]);
     }
